42046407514733e0e2a153264b5011653f533e92
Maxi Redigonda <maxiredigonda@gmail.com>
2018-10-05 12:47:25 -0300

    modified get_language_data to consider line numbers and created add_property method

--- main.py
+++ main.py
@@ -22,16 +22,45 @@ class Translater:
         return lang
     
     def get_language_data(self, lang):
+        data = {}
         with open(lang + '.js', encoding='utf-8') as f:
-            language_description = self.get_language_description(f.read())
-            data = ast.literal_eval(language_description)
-            return data
+            lineno = 0
+            for l in f:
+                lineno += 1
+                if l[0] == 'e':
+                    continue # export default line
+                if l[0] == '}':
+                    continue # last line
+                line_data = ast.literal_eval('{'+l+'}')
+                if line_data.keys():
+                    key = list(line_data.keys())[0]
+                    data[key] = {
+                        'value': line_data[key], 
+                        'lineno': lineno,
+                    }
+        return data
+
+    def add_property(self, lang, key, value, line):
+        """Adds a property to a lang.js file.
+        """
+        f = open(lang + '.js', 'r')
+        contents = f.readlines()
+        f.close()
+        
+        new_line = "    '{0}': '{1}',\n".format(key, value)
+        #~ print(new_line)
+        contents.insert(line - 1, new_line)
+        
+        f = open(lang + '.js', 'w')
+        contents = "".join(contents)
+        f.write(contents)
+        f.close()
 
     def main(self):
         translator = Translator()
         #~ language_list = ['br', 'cn', 'de', 'es', 'fr', 'gr', 'in', 'it', 
             #~ 'jp', 'nl', 'pt', 'ru', 'tr']
-        language_list = ['br', 'cn']
+        language_list = ['br']
             
         for language in language_list:
             print('Translating for language: ' + language)
@@ -40,8 +69,9 @@ class Translater:
             
             for key in self.data.keys():
                 if not key in odata:
-                    value = translator.translate(self.data[key], src='en', 
-                                                 dest=dest_language).text
+                    value = translator.translate(self.data[key]['value'], 
+                        src='en', dest=dest_language).text
+                    self.add_property(language, key, value, self.data[key]['lineno'])
                     print("'"+key+"': '"+value+"',")
 
 if __name__ == "__main__":
